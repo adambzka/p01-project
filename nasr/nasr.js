e84745b6384aa78da76de79d7363b87bc17e3b60
Nasreddine887 <302245681@student.rocmondriaan.nl>
2025-10-03 10:24:06 +0200

    js en html veranderd

--- nasr/nasr.js
+++ nasr/nasr.js
@@ -1,9 +1,10 @@
-
-const button = document.querySelector('.container');
-
-const elementToRemove = document.querySelector('.start-btn');
+const button = document.querySelector('.start-btn');
+const elementToRemove = document.querySelector('.container');
+const div2 = document.querySelector('.div-2');
+const questionContainer = document.querySelector('.question-container');
 
 button.addEventListener('click', () => {
-  
-  elementToRemove.remove();
+  elementToRemove.style.display = 'none';
+  div2.style.display = 'flex';
+  questionContainer.style.display = 'flex';
 });
