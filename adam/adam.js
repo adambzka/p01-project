8f0e31ab24e0b4ff0bce58001a65a05a5ffa7063
adambzka <302255327@student.rocmondriaan.nl>
2025-10-07 16:45:37 +0200

    !!!

---
+++ adam/adam.js
@@ -0,0 +1,22 @@
+const vragen = [
+    {
+        vraag: "In welk jaar eindigde de tweede wereld oorlog?"
+        antwoorden: [
+            { text: "1933", correct: false},
+            { text: "1965", correct: false},
+            { text: "1944", correct: false},
+            { text: "1945", correct: true},
+        ] 
+    },
+    {
+        vraag: "In welk jaar eindigde de tweede wereld oorlog?"
+        antwoorden: [
+            { text: "1933", correct: false},
+            { text: "1965", correct: false},
+            { text: "1944", correct: false},
+            { text: "1945", correct: true},
+        ] 
+    }
+];
+
+const vraagElement = document.getElementById("vraag");
